e6a82ff1c25501c50e683b3e3614db6a882ef212
gitBugD <diana.laurenti@heig-vd.ch>
2022-10-17 17:06:17 +0200

    Commit test

---
+++ cmake-build-debug/CMakeFiles/Lab02.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab02.dir/main.cpp.obj"
+  "CMakeFiles/Lab02.dir/main.cpp.obj.d"
+  "Lab02.exe"
+  "Lab02.exe.manifest"
+  "Lab02.pdb"
+  "libLab02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
